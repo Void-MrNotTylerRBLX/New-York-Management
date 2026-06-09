7b1f9e33799ffc7dddb6f969ec88407c4a22eba4
Void-MrNotTylerRBLX <tylertruongtrd@gmail.com>
2026-06-09 17:47:44 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -89,7 +89,7 @@ export const botConfig = {
     colors: {
       // Main brand colors.
       primary: "#336699", 
-      secondary: "#2F3136", 
+      secondary: "#BBE2FC", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
